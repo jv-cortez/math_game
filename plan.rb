f67e181ad2d42fcb7909337f38c546bd19a919cc
Juan Victor Cortez <juanvictor.cortez@gmail.com>
2017-08-02 00:16:27 +0000

    created game plan for math game execution

---
+++ plan.rb
@@ -0,0 +1,19 @@
+# Player
+# This class will store information about the players including their lives their actions 
+  # (store) create players
+  # (store) player lives
+  # (behaviour) evaluate player answers
+  # (behaviour) Will manage the current player
+
+# Game logic
+  # (store) actions when players answer such as losing lives and losing the game
+  # (behaviour) the behaviour for losing and winning the game
+  # (behaviour) will contain the game loop
+
+# Game rules
+  # (store) information on how players lose lives
+  # (store) game questions and corresponding answers
+
+  
+  #refactor more
+  # game rules and game logic seem to be the same
